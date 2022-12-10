f8e829044cbd6cf809cb8cb55488f02e68303d9f
Luiz Calaça <luizcalaca@gmail.com>
2022-12-10 07:48:24 -0300

    fix: adding necessary elements on todo.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
-import { TodoController } from './tod/controller/todo.controller';
+import { TodoController } from './todo/controller/todo.controller';
+import { TodoService } from './todo/service/todo.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [TodoModule],
   controllers: [AppController, TodoController],
-  providers: [AppService],
+  providers: [AppService, TodoService, PrismaService],
 })
 export class AppModule {}
--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TodoService } from './service/todo.service';
 import { TodoController } from './controller/todo.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [TodoService],
+  imports: [TodoModule],
   controllers: [TodoController],
+  providers: [TodoService, PrismaService],
 })
 export class TodoModule {}
